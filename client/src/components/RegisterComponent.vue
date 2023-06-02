c9134f5e7868194b14faa6b7b6d4f234d4da3b08
anisimovadim <anisimov.vadim.alexeevich@mail.ru>
2023-06-03 00:00:37 +0300

    update project

---
+++ client/src/components/RegisterComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email"
+           name="email"
+           v-model="email"
+           placeholder="email">
+    <input type="password"
+           name="password"
+           v-model="password"
+           placeholder="password">
+    <button @click="register">Зарегистрироваться</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+</style>
